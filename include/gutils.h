406ca6c7cb68f249b1830fc5c442b35553aa6567
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-10-09 10:08:18 -0400

    Add: debugging helper functions

--- include/gutils.h
+++ include/gutils.h
@@ -1,7 +1,32 @@
 #ifndef INCLUDED_GUTILS
 #define INCLUDED_GUTILS
 
+#include <iostream>
 #include <string>
+#include <unistd.h>
+
+#define dmsg(x) \
+    if (debugging_enabled) { \
+        std::cout << "[DEBUG] " << x << std::endl; \
+    }
+
+#define dvmsg(x) dmsg(#x ": " << (x))
+
+static bool debugging_enabled = false;
+
+inline void set_debug_mode(bool debug_flag) {
+    debugging_enabled = debug_flag;
+    dmsg("Debug Mode Enabled");
+}
+
+inline void die(const std::string msg, int ec) {
+    std::cout << msg << std::endl;
+    exit(ec);
+}
+
+inline void die(const std::string msg) {
+    die(msg, 1);
+}
 
 namespace gutils {
 
--- src/gutils.cc
+++ src/gutils.cc
@@ -1,6 +1,5 @@
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 
 #include <gutils.h>
 
